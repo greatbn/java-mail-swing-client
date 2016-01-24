3f1336b6205121d5abceeb643dbed70316cac48b
Trung Quân <quanlightning17@gmail.com>
2016-01-24 12:52:04 +0700

    test ok

---
+++ mail-client/src/Controls/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controls;
+
+/**
+ *
+ * @author VS9 X64Bit
+ */
+public class NewClass {
+    
+}
